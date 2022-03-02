a408e8128adb92d1b2436bbce4a17a458ee2fff1
Daniel Matthews <mr.drmatthews@gmail.com>
2022-03-02 00:38:01 +0000

    comments

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -9,22 +9,32 @@ namespace Generics
         {
 
             string myNameSpace = "Generics";
-            string myClassName = "Dog";
+            string myClassName = "Cat";
 
-            
-            Type[] typeArguments = { Type.GetType(myNameSpace + "." + myClassName) };
+            // The type used to instantiate the generic class ie genericClass<T> ... this is the <T>
+            Type myClassType = Type.GetType(myNameSpace + "." + myClassName);
+
+            // an array of types needed for the generic class, 
+            //if we needed more than one they go in here in the order of the class definition
+            Type[] typeArguments = { myClassType };
+
+            // The Class with the <T> definition we want to instantiate
             Type myGenericClassType = typeof(MyGenericClass<>);
             
-
+            // creates the class with the correct types
             Type constructed = myGenericClassType.MakeGenericType(typeArguments);
+
+            // gets a method/function from the created class to run
             MethodInfo methodExample1 = constructed.GetMethod("Example1");
             MethodInfo methodExample2 = constructed.GetMethod("Example2");
+
+            //instantiates the created class - this will run the constructor (with no parametors)
             var myGenericClassInstance = Activator.CreateInstance(constructed);
 
             // run method with params
-            var result = methodExample1.Invoke(myGenericClassInstance, new object[] { "Hello" });
+            methodExample1.Invoke(myGenericClassInstance, new object[] { "Hello" });
 
-            // run method without params
+            // run method without params, also casting the result to same return type as the method
             string result2 = (string)methodExample2.Invoke(myGenericClassInstance, null);
             Console.WriteLine(result2);
         }
